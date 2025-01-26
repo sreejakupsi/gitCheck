354f0ed8be0d6b1cd5b35ba6004b49d434731efd
sreejakupsi <sreejakumar2907@gmail.com>
2025-01-26 16:08:59 +0530

    updated changes

--- src/gitPractice/Basic.java
+++ src/gitPractice/Basic.java
@@ -5,6 +5,15 @@ public class Basic {
 	public static void main(String[] args) {
 		System.out.println("Hello Sathish");
 		System.out.println("New changes");
+		int i=10;
+		int j=90;
+		int result=i+j;
+		System.out.println(result);
+		
+		
+		for (int i1=0;i1<=10;i1++) {
+			System.out.println(i1);
+		}
 	}
 
 }
